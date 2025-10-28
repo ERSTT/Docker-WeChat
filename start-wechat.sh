d890cb75e582387f6d0a9571546beb3288fdbc7f
ERST <158469810+ERSTT@users.noreply.github.com>
2025-10-28 20:51:43 +0800

    Update start-wechat.sh

--- start-wechat.sh
+++ start-wechat.sh
@@ -3,12 +3,12 @@
 MARKER="/usr/local/wechat_installed"
 
 if [ ! -f "$MARKER" ]; then
-    curl -L -o /tmp/WeChatLinux_x86_64.deb \
+    sudo curl -L -o /tmp/WeChatLinux_x86_64.deb \
         https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb && \
-    apt-get install -y /tmp/WeChatLinux_x86_64.deb && \
-    rm -rf /tmp/WeChatLinux_x86_64.deb
+    sudo apt-get install -y /tmp/WeChatLinux_x86_64.deb && \
+    sudo rm -rf /tmp/WeChatLinux_x86_64.deb
 
-    touch "$MARKER"
+    sudo touch "$MARKER"
 fi
 
 /usr/bin/wechat > /dev/null 2>&1 &
