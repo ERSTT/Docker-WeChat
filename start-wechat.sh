3e1df536c8d264c26d2ad3bdb426883cbdc6c9ac
ERST <158469810+ERSTT@users.noreply.github.com>
2025-10-28 20:38:43 +0800

    Implement WeChat installation check in script
    Add installation script for WeChat if not installed

--- start-wechat.sh
+++ start-wechat.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
+
+MARKER="/usr/local/wechat_installed"
+
+if [ ! -f "$MARKER" ]; then
+    curl -L -o /tmp/WeChatLinux_x86_64.deb \
+        https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb && \
+    apt-get install -y /tmp/WeChatLinux_x86_64.deb && \
+    rm -rf /tmp/WeChatLinux_x86_64.deb
+
+    touch "$MARKER"
+fi
+
 /usr/bin/wechat > /dev/null 2>&1 &
