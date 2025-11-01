581c3e1e60f0d190627db42436e1303cfa50830b
ERST <158469810+ERSTT@users.noreply.github.com>
2025-11-01 11:24:47 +0800

    Rename marker variable for QQ installation check

--- start-qq.sh
+++ start-qq.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-MARKER="/usr/local/qq_installed"
+QQ="/usr/local/qq_installed"
 
-if [ ! -f "$MARKER" ]; then
+if [ ! -f "$QQ" ]; then
     sudo curl -L -o /tmp/QQLinux_x86_64.deb \
         https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.20_251023_amd64_01.deb && \
     sudo apt-get install -y /tmp/QQLinux_x86_64.deb && \
     sudo rm -rf /tmp/QQLinux_x86_64.deb
 
-    sudo touch "$MARKER"
+    sudo touch "$QQ"
 fi
 
 /usr/bin/wechat > /dev/null 2>&1 &
