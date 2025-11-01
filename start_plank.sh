fb5a2627ca5d8405aaa11d07d17a1a0b296ee0e1
ERST <158469810+ERSTT@users.noreply.github.com>
2025-11-01 14:00:04 +0800

    Create start_plank.sh

---
+++ start_plank.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+plank > /dev/null 2>&1 &
+
+prev_res=$(xrandr | grep '*' | awk '{print $1}')
+
+while true; do
+    sleep 2
+    curr_res=$(xrandr | grep '*' | awk '{print $1}')
+    if [ "$curr_res" != "$prev_res" ]; then
+        pkill plank
+        sleep 1
+        plank > /dev/null 2>&1 &
+        prev_res=$curr_res
+    fi
+done
