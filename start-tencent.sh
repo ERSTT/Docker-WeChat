4f517f6a505f1b8c1450160aeb5717dd8a430148
ERST <158469810+ERSTT@users.noreply.github.com>
2025-11-01 11:01:05 +0800

    Create start-tencent.sh

---
+++ start-tencent.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+QQ="/usr/local/qq_installed"
+WeChat="/usr/local/wechat_installed"
+
+if [ ! -f "$QQ" ]; then
+    sudo curl -L -o /tmp/QQLinux_x86_64.deb \
+        https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.20_251023_amd64_01.deb && \
+    sudo apt-get install -y /tmp/QQLinux_x86_64.deb && \
+    sudo rm -rf /tmp/QQLinux_x86_64.deb
+
+    sudo touch "$QQ"
+fi
+
+if [ ! -f "$WeChat" ]; then
+    sudo curl -L -o /tmp/WeChatLinux_x86_64.deb \
+        https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb && \
+    sudo apt-get install -y /tmp/WeChatLinux_x86_64.deb && \
+    sudo rm -rf /tmp/WeChatLinux_x86_64.deb
+
+    sudo touch "$WeChat"
+fi
+
+/usr/bin/wechat > /dev/null 2>&1 &
+/usr/bin/qq > /dev/null 2>&1 &
