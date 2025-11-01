a7642802e21ec2976c20fccd19414537197b1839
ERST <158469810+ERSTT@users.noreply.github.com>
2025-11-01 14:36:09 +0800

    Update start-plank.sh

--- start-plank.sh
+++ start-plank.sh
@@ -4,18 +4,26 @@ trap '' SIGHUP SIGTERM
 
 plank > /dev/null 2>&1 &
 
-prev_res=$(xrandr | grep '*' | awk '{print $1}')
+get_resolutions() {
+    xrandr | grep ' connected' | awk '{print $1,$3}' | sed 's/+.*//'
+}
 
-# 无限循环，但不占用 CPU
+prev_res=$(get_resolutions)
+
 while true; do
-    xrandr --verbose | while read -r line; do
-        curr_res=$(xrandr | grep '*' | awk '{print $1}')
-        if [ "$curr_res" != "$prev_res" ]; then
-            pkill plank
-            sleep 0.5
-            plank > /dev/null 2>&1 &
-            prev_res=$curr_res
-            break
-        fi
-    done
+    xrandr --verbose > /tmp/xrandr_tmp 2>/dev/null
+    
+    curr_res=$(get_resolutions)
+    
+    if [ "$curr_res" != "$prev_res" ]; then
+        pkill plank
+        
+        while pgrep plank > /dev/null; do
+            sleep 0.1
+        done
+        
+        plank > /dev/null 2>&1 &
+        
+        prev_res="$curr_res"
+    fi
 done
