a20ad95b3a0e8d492c43a085a000b3ba7a5dc700
ERST <158469810+ERSTT@users.noreply.github.com>
2025-11-01 12:12:32 +0800

    Update start-wxqq.sh

--- start-wxqq.sh
+++ start-wxqq.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
-QQ="/usr/local/qq_installed"
 WeChat="/usr/local/wechat_installed"
-
-if [ ! -f "$QQ" ]; then
-    sudo curl -L -o /tmp/QQLinux_x86_64.deb \
-        https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.20_251023_amd64_01.deb && \
-    sudo apt-get install -y /tmp/QQLinux_x86_64.deb && \
-    sudo rm -rf /tmp/QQLinux_x86_64.deb
-
-    sudo touch "$QQ"
-fi
+QQ="/usr/local/qq_installed"
 
 if [ ! -f "$WeChat" ]; then
     sudo curl -L -o /tmp/WeChatLinux_x86_64.deb \
@@ -19,6 +10,15 @@ if [ ! -f "$WeChat" ]; then
     sudo rm -rf /tmp/WeChatLinux_x86_64.deb
 
     sudo touch "$WeChat"
+fi
+
+if [ ! -f "$QQ" ]; then
+    sudo curl -L -o /tmp/QQLinux_x86_64.deb \
+        https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.20_251023_amd64_01.deb && \
+    sudo apt-get install -y /tmp/QQLinux_x86_64.deb && \
+    sudo rm -rf /tmp/QQLinux_x86_64.deb
+
+    sudo touch "$QQ"
 fi
 
 /usr/bin/wechat > /dev/null 2>&1 &
