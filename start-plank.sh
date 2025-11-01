7601f3ada651d8712b08cd753155159942812af2
ERST <158469810+ERSTT@users.noreply.github.com>
2025-11-01 14:32:43 +0800

    Update start-plank.sh

--- start-plank.sh
+++ start-plank.sh
@@ -6,13 +6,16 @@ plank > /dev/null 2>&1 &
 
 prev_res=$(xrandr | grep '*' | awk '{print $1}')
 
+# 无限循环，但不占用 CPU
 while true; do
-    sleep 2
-    curr_res=$(xrandr | grep '*' | awk '{print $1}')
-    if [ "$curr_res" != "$prev_res" ]; then
-        pkill plank
-        sleep 1
-        plank > /dev/null 2>&1 &
-        prev_res=$curr_res
-    fi
+    xrandr --verbose | while read -r line; do
+        curr_res=$(xrandr | grep '*' | awk '{print $1}')
+        if [ "$curr_res" != "$prev_res" ]; then
+            pkill plank
+            sleep 0.5
+            plank > /dev/null 2>&1 &
+            prev_res=$curr_res
+            break
+        fi
+    done
 done
