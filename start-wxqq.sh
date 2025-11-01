01bc22372f212d14b0ece598c5fe203de1a53fb6
ERST <158469810+ERSTT@users.noreply.github.com>
2025-11-01 13:03:47 +0800

    Update start-wxqq.sh

--- start-wxqq.sh
+++ start-wxqq.sh
@@ -4,23 +4,25 @@ WeChat="/usr/local/wechat_installed"
 QQ="/usr/local/qq_installed"
 
 if [ ! -f "$WeChat" ]; then
-    echo install WeChat && \
-    sudo curl -L -o /tmp/WeChatLinux_x86_64.deb \
-        https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb && \
-    sudo apt-get install -y /tmp/WeChatLinux_x86_64.deb && \
-    sudo rm -rf /tmp/WeChatLinux_x86_64.deb >> /proc/1/fd/1 2>&1
-
-    sudo touch "$WeChat"
+    {
+        echo "Install WeChat"
+        sudo curl -L -o /tmp/WeChatLinux_x86_64.deb \
+            https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb
+        sudo apt-get install -y /tmp/WeChatLinux_x86_64.deb
+        sudo rm -rf /tmp/WeChatLinux_x86_64.deb
+        sudo touch "$WeChat"
+    } >> /proc/1/fd/1 2>&1
 fi
 
 if [ ! -f "$QQ" ]; then
-    echo install QQ && \
-    sudo curl -L -o /tmp/QQLinux_x86_64.deb \
-        https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.20_251023_amd64_01.deb && \
-    sudo apt-get install -y /tmp/QQLinux_x86_64.deb && \
-    sudo rm -rf /tmp/QQLinux_x86_64.deb >> /proc/1/fd/1 2>&1
-
-    sudo touch "$QQ"
+    {
+        echo "Install QQ"
+        sudo curl -L -o /tmp/QQLinux_x86_64.deb \
+            https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.20_251023_amd64_01.deb
+        sudo apt-get install -y /tmp/QQLinux_x86_64.deb
+        sudo rm -rf /tmp/QQLinux_x86_64.deb
+        sudo touch "$QQ"
+    } >> /proc/1/fd/1 2>&1
 fi
 
 sleep 2
