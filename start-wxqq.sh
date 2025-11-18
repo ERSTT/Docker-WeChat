72aaff56fc38c09f761fddbabee059bc46b9e6ed
ERST <158469810+ERSTT@users.noreply.github.com>
2025-11-18 20:13:17 +0800

    Update start-wxqq.sh

--- start-wxqq.sh
+++ start-wxqq.sh
@@ -55,7 +55,7 @@ fi
 if [ ! -f "$QQ" ]; then
     echo "Install QQ" | tee -a "$QQLog"
     sudo curl -L -o /tmp/QQLinux_x86_64.deb \
-        https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.20_251023_amd64_01.deb 2>&1 | tee -a "$QQLog"
+        https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.21_251114_amd64_01.deb 2>&1 | tee -a "$QQLog"
     sudo apt-get install -y /tmp/QQLinux_x86_64.deb 2>&1 | tee -a "$QQLog"
     rm -rf /tmp/QQLinux_x86_64.deb 2>&1 | tee -a "$QQLog"
     sudo touch "$QQ" 2>&1 | tee -a "$QQLog"
