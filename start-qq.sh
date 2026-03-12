446d0a78b126ffe92be098c1ab083aaa3bd3aa59
ERST <158469810+ERSTT@users.noreply.github.com>
2026-03-12 19:02:05 +0800

    Update QQ download link in start-qq.sh

--- start-qq.sh
+++ start-qq.sh
@@ -6,7 +6,7 @@ QQLog="/config/log/QQ.log"
 if [ ! -f "$QQ" ]; then
     echo "Install QQ" | tee -a "$QQLog"
     sudo curl -L -o /tmp/QQLinux_x86_64.deb \
-        https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.26_260311_amd64_01.deb 2>&1 | tee -a "$QQLog"
+        https://dldir1v6.qq.com/qqfile/qq/QQNT/d3f1a1be/linuxqq_3.2.26-46494_amd64.deb 2>&1 | tee -a "$QQLog"
     sudo apt-get install -y /tmp/QQLinux_x86_64.deb 2>&1 | tee -a "$QQLog"
     rm -rf /tmp/QQLinux_x86_64.deb 2>&1 | tee -a "$QQLog"
     sudo touch "$QQ" 2>&1 | tee -a "$QQLog"
